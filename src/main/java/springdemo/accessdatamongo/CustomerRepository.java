dce29bda4b4c558f9166e7a3bbaab526dff3396c
FCA-00015\fintech <hungnguyenuet@gmail.com>
2021-03-24 09:38:03 +0700

    restructure package

---
+++ src/main/java/springdemo/accessdatamongo/CustomerRepository.java
@@ -0,0 +1,12 @@
+package hungnv.springdemo.accessdatamongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstname);
+
+    public List<Customer> findByLastName(String lastName);
+}
---
+++ src/main/java/springdemo/uploadingfiles/StorageFileNotFoundException.java
@@ -0,0 +1,4 @@
+package hungnv.springdemo.uploadingfiles;
+
+public class StorageFileNotFoundException extends Exception{
+}
