a51657d46a834caa6dfea7db6f32ac1368ded9d4
FCA-00015\fintech <hungnguyenuet@gmail.com>
2021-03-25 17:51:23 +0700

    Authenticating Ldap

---
+++ src/main/java/springdemo/authenticatingldap/AuthenticatingLdapApplication.java
@@ -0,0 +1,11 @@
+package springdemo.authenticatingldap;
+
+import org.springframework.boot.SpringApplication;
+
+public class AuthenticatingLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticatingLdapApplication.class, args);
+    }
+
+}
